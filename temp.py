be41f01e35fb1b6d48b3e2673eb3ca7bf64b270f
Marc Lamberts <43936834+marclamberts@users.noreply.github.com>
2025-05-30 07:46:32 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,14 +1,14 @@
 
 import streamlit as st
-from mplsoccer import Pitch
+from mplsoccer import VerticalPitch
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Vertical Half Pitch with mplsoccer", layout="centered")
 st.title("⚽ Vertical Half Pitch (Using mplsoccer)")
 
 # Create a vertical half pitch
-pitch = Pitch(pitch_type='statsbomb', pitch_color='#144A29',
-              line_color='white', half=True, orientation='vertical')
+pitch = VerticalPitch(pitch_type='statsbomb', pitch_color='#144A29',
+              line_color='white', half=True)
 
 fig, ax = pitch.draw(figsize=(6, 8))
 
