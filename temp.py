90a2063efaa35b00b2adc56553876fa2c269d081
Marc Lamberts <43936834+marclamberts@users.noreply.github.com>
2025-06-03 14:54:49 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,8 +5,6 @@ import os
 import ast
 import plotly.express as px
 import plotly.graph_objects as go
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
